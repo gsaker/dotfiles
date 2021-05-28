c841ff4cf745f06d021bfd5091726d51d1ba5e1f
Kriss Wiggins <cd.wiggins@outlook.com>
2021-05-28 18:38:58 +0100

    Added MaterialFox installation script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-copy() {
-	echo "Installing; this may take some time..."
+materialfox="https://github.com/muckSponge/MaterialFox"
 
+copy() {
 	if [[ ! -d "$HOME"/.config ]]; then mkdir -p "$HOME"/.config; fi
 	cp -R config/* "$HOME"/.config
 	
@@ -17,22 +17,78 @@ copy() {
 
 	if [[ ! -d "$HOME"/.themes ]]; then mkdir -p "$HOME"/.themes; fi
 	cp -R themes/* "$HOME"/.themes
-	
+
 	cp .zshrc .bashrc .profile .p10k.zsh "$HOME"
 	chmod +x "$HOME"/scripts/*
+}
+
+postInstall() {
 	scripts/setbg "$HOME"/Pictures/Wallpapers/default
 	fc-cache -f
+}
 
-	echo "Installed. Run \`setbg <path to image>\` to change your desktop background."
+firefoxConfig() {
+	ff="$HOME"/.mozilla/firefox
+	if [[ -d "$ff" ]]; then
+		root_dir=$(pwd)
+		ff_root=$(ls "$HOME"/.mozilla/firefox)
+
+		cd "$ff"
+		profiles=$(find ./ -type d -name "*.default-release")
+		
+		if [[ -n $profiles ]]; then
+			for profile in $profiles; do
+				cp "$root_dir"/firefox/user.js "./$profile"
+			done
+			
+			if [[ -d ./materialfox  ]]; then
+				echo "Using existing MaterialFox clone"
+			else
+				git clone "$materialfox" ./materialfox
+			fi
+			
+			if [[ -d ./materialfox/chrome ]]; then
+				for profile in $profiles; do
+					cp -R ./materialfox/chrome "./$profile"
+				done
+			else
+				echo "Skipping MaterialFox installation"
+			fi
+		else
+			echo "No suitable Firefox profiles found."
+		fi
+
+		cd "$root_dir"
+	else
+		echo "Firefox config directory \"$ff\" not found."
+	fi
+}
+
+run() {
+	echo "Installing; this may take some time..."
+
+	copy
+	postInstall
+
+	echo "Install the MaterialFox theme by muckSponge and smooth scrolling config for Firefox?"
+	echo "Note: This will overwrite the Firefox \"user.js\" and \"chrome\" directory files."
+	printf "Proceed? (y/N) "
+	read res
+	if [[ "$res" == "y" ]]; then
+		echo "Installing Firefox configuration..."
+		firefoxConfig
+	fi
+
+	echo "Installed dotfiles. Run \`setbg <path to image>\` to change your desktop background."
 	echo "You may now use a tool such as lxappearance to customise GTK and other theming options"
 }
 
 echo "Make sure that you have read the readme and installed the required dependencies."
-echo "This will overwrite certain files and therefore data loss may occur when installing this configuration."
+echo "This will overwrite various configuration files. Don't proceed if you have any configurations that you have not backed up."
 printf "Proceed? (y/N) "
 read res
 if [[ "$res" == "y" ]]; then
-	copy
+	run
 else
 	echo "No changes made. Exiting..."
 	exit 0
